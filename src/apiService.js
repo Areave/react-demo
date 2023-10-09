d095805aa1f63032dd30974a6215eefa761653f2
Хохлушин Игорь <just4-spam@mail.ru>
2023-10-09 12:37:28 +0600

    ert

--- src/apiService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const getListOfAvailableCurrencies = () => axios.get(getListOfAvailableCurrenciesUrl)
-    .then(data => data.data)
-    .catch(error => error);
-
-export default {
-    getListOfAvailableCurrencies,
-}
--- src/comps/ButtonComp/ButtonComp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import './ButtonCompStyle.scss'
-
-const ButtonComp = ({label, callback}) => {
-    return <Button className='exchange_button' onClick={callback}>{label}</Button>
-};
-
-export default ButtonComp;
