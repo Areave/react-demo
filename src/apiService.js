e8ca04938b79ea0cf1a3f3b81c338176ce619c21
Хохлушин Игорь <just4-spam@mail.ru>
2023-10-09 12:25:15 +0600

    456

--- src/apiService.js
+++ src/apiService.js
@@ -1,26 +1,9 @@
 import axios from 'axios'
 
-const apiKey = 'c9155859d90d239f909d2906233816b26cd8cf5ede44702d422667672b58b0cd';
-const getListOfAvailableCurrenciesUrl = 'https://api.changenow.io/v1/currencies?active=true&fixedRate=true';
-
 const getListOfAvailableCurrencies = () => axios.get(getListOfAvailableCurrenciesUrl)
-    .then(data => data.data)
-    .catch(error => error);
-
-const getMinimalExchangeAmount = (from_to) => axios.get(`https://api.changenow.io/v1/min-amount/${from_to}?api_key=${apiKey}`)
-    .then(data => data.data)
-    .catch(error => {
-        const errorMessage = error.response.data.error;
-        const errorMessageString = errorMessage.split('_').join(' ');
-        throw new Error(errorMessageString);
-    });
-
-const getEstimatedExchangeAmount = (from_to, amount) => axios.get(`https://api.changenow.io/v1/exchange-amount/${amount}/${from_to}/?api_key=${apiKey}`)
     .then(data => data.data)
     .catch(error => error);
 
 export default {
     getListOfAvailableCurrencies,
-    getMinimalExchangeAmount,
-    getEstimatedExchangeAmount
 }
